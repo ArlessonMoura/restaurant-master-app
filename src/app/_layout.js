6133be942b57caded9e30fa6f908bc86732e74fd
ArlessonMoura <arlessonmss@gmail.com>
2025-02-01 09:21:56 -0300

    🎉 Starting project

---
+++ src/app/_layout.js
@@ -0,0 +1,7 @@
+import { Stack, Slot } from 'expo-router';
+
+function RootLayout() {
+  return <Stack></Stack>;
+}
+
+export default RootLayout;
---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'red' }}>App</Text>
+    </View>
+  );
+}
+
+export default App;
